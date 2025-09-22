2a750e19087a8120edb275dd0fb1954857a2e5ef
sulenq <fatwalinovera@gmail.com>
2025-09-22 20:54:21 +0700

    styling

--- src/components/widget/PartnersLogo.tsx
+++ src/components/widget/PartnersLogo.tsx
@@ -1,40 +1,45 @@
 "use client";
 
-import { Img } from "@/components/ui/img";
-import { IMAGES_PATH } from "@/constants/paths";
-import { HStack, StackProps } from "@chakra-ui/react";
+import { CContainer } from "@/components/ui/c-container";
+import { P } from "@/components/ui/p";
+import { StackProps } from "@chakra-ui/react";
 
 export const PartnersLogo = (props: StackProps) => {
   // Props
   const { ...restProps } = props;
 
   return (
-    <HStack {...restProps}>
-      <Img
-        src={`${IMAGES_PATH}/atrbpn-logo.png`}
-        alt="ATR BPN Logo"
-        w={"auto"}
-        h={restProps?.h || "30px"}
-        aspectRatio={1}
-        objectFit="contain"
-      />
-      <Img
-        src={`${IMAGES_PATH}/unep-logo.png`}
-        alt="UNEP Logo"
-        w={"auto"}
-        h={restProps?.h || "30px"}
-        aspectRatio={1}
-        objectFit="contain"
-        ml={"6px"}
-      />
-      <Img
-        src={`${IMAGES_PATH}/gef-logo.png`}
-        alt="gef Logo"
-        w={"auto"}
-        h={restProps?.h || "30px"}
-        aspectRatio={1}
-        objectFit="contain"
-      />
-    </HStack>
+    <CContainer color={"p.500"} {...restProps}>
+      <P fontWeight={"medium"}>Program Koridor</P>
+      <P fontWeight={"medium"}>Ekosistem RIMBA</P>
+    </CContainer>
+
+    // <HStack {...restProps}>
+    //   <Img
+    //     src={`${IMAGES_PATH}/atrbpn-logo.png`}
+    //     alt="ATR BPN Logo"
+    //     w={"auto"}
+    //     h={restProps?.h || "30px"}
+    //     aspectRatio={1}
+    //     objectFit="contain"
+    //   />
+    //   <Img
+    //     src={`${IMAGES_PATH}/unep-logo.png`}
+    //     alt="UNEP Logo"
+    //     w={"auto"}
+    //     h={restProps?.h || "30px"}
+    //     aspectRatio={1}
+    //     objectFit="contain"
+    //     ml={"6px"}
+    //   />
+    //   <Img
+    //     src={`${IMAGES_PATH}/gef-logo.png`}
+    //     alt="gef Logo"
+    //     w={"auto"}
+    //     h={restProps?.h || "30px"}
+    //     aspectRatio={1}
+    //     objectFit="contain"
+    //   />
+    // </HStack>
   );
 };

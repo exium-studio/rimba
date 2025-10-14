4807e9586a0e7c90e7e0755fd0a181e52cf1082f
sulenq <fatwalinovera@gmail.com>
2025-10-14 16:48:15 +0700

    styling

--- src/components/widget/EditableContentContainer.tsx
+++ src/components/widget/EditableContentContainer.tsx
@@ -1,24 +1,33 @@
+"use client";
+
 import { CContainer } from "@/components/ui/c-container";
 import { P } from "@/components/ui/p";
 import { Props__EditableContentContainer } from "@/constants/props";
-import useCMS from "@/context/useCMS";
 import { Center } from "@chakra-ui/react";
+import { useSearchParams } from "next/navigation";
 
-export const EditableContentContainer = (
+export function EditableContentContainer(
   props: Props__EditableContentContainer
-) => {
+) {
   // Props
   const { children, content, ...restProps } = props;
 
-  // Contexts
-  const highlightedContentIds = useCMS((s) => s.highlightedContentIds);
-  const isHighlighted = highlightedContentIds.includes(content.id);
+  // Hooks
+  const searchParams = useSearchParams();
+
+  // Parse `highlightedContentIds` from URL
+  const highlightedContentIdsParam = searchParams.get("highlightedContentIds");
+  const highlightedContentIds = highlightedContentIdsParam
+    ? highlightedContentIdsParam.split(",").map((id) => id.trim())
+    : [];
+
+  const isHighlighted = highlightedContentIds.includes(String(content.id));
 
   return (
     <CContainer
       id={`content_${content.id}`}
-      w={"fit"}
-      pos={"relative"}
+      w="fit"
+      pos="relative"
       {...restProps}
     >
       {children}
@@ -26,26 +35,27 @@ export const EditableContentContainer = (
       {isHighlighted && (
         <>
           <Center
-            w={"full"}
-            h={"full"}
-            bg={isHighlighted ? "orange.400" : ""}
-            // bg={isHighlighted ? "orange.400" : "orange.400"} //! debug
+            w="full"
+            h="full"
+            bg="orange.400"
             opacity={0.8}
-            pos={"absolute"}
+            pos="absolute"
           />
 
           <P
-            fontSize={"xl"}
-            fontWeight={"bold"}
-            whiteSpace={"nowrap"}
-            color={"light"}
-            pos={"absolute"}
-            top={"50%"}
-            left={"50%"}
-            transform={"translate(-50%, -50%)"}
-          >{`${content.id}`}</P>
+            fontSize="xl"
+            fontWeight="bold"
+            whiteSpace="nowrap"
+            color="light"
+            pos="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          >
+            {content.id}
+          </P>
         </>
       )}
     </CContainer>
   );
-};
+}
